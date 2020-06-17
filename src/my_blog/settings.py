b79962a25fac9e9d44eed65cc719b314bd050566
simo684 <attabousimo@hotmail.fr>
2020-06-17 17:20:25 +0100

    new project + 2apps

--- src/my_blog/settings.py
+++ src/my_blog/settings.py
@@ -122,6 +122,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 #STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR , "static" ),
-#    '/home/attabou/Bureau/myprojects/DjangoProject/src/static',
+#   os.path.join(BASE_DIR , "static" ),
+#  '/home/attabou/Bureau/myprojects/DjangoProject/src/static',
 #]
